24c6fd24ff52be94082fb062d872685cea105533
m._neto <reidogado499@gmail.com>
2023-04-09 10:23:40 -0300

    cors add

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'
 
 import connectDB from './configs/mongodb';
 import router from './routers/shortLinkRouters';
@@ -6,6 +7,7 @@ import router from './routers/shortLinkRouters';
 const app = express();
 connectDB();
 
+app.use(cors())
 app.use(express.json())
 app.use("/shortlink", router);
 
